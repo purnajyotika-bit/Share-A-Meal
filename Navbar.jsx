3e845ed71fdfa8d2542a341d4e350ab601bd5411
purnajyotika-bit <purnajyotika@gmail.com>
2026-06-02 20:50:53 +0530

    Replace SVG icon in Navbar component

--- Navbar.jsx
+++ Navbar.jsx
@@ -35,13 +35,8 @@ export default function Navbar() {
           
           <Link to="/" className="flex items-center gap-3 transition-transform hover:scale-105">
             <div className={`relative h-12 w-12 overflow-hidden rounded-full border-2 ${logoBorderColor} bg-orange-500 flex items-center justify-center shadow-lg hover:border-orange-300`}>
-              <svg viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="h-[60%] w-[60%]">
-                {/* Simple Bowl */}
-                <path d="M5 9h14l-2 9H7l-2-9z" />
-                {/* Minimalist Spoon */}
-                <path d="M16 6l-5 5" />
-                {/* Heart Steam */}
-                <path d="M12 4c.6-1 1.5-.5 1.5.5 0 .8-.8 1.3-1.5 2-.7-.7-1.5-1.2-1.5-2 0-1 .9-1.5 1.5-.5z" fill="white" />
+              <svg viewBox="0 0 24 24" fill="white" stroke="white" strokeWidth="2" className="h-[50%] w-[50%]">
+                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
               </svg>
             </div>
             <div className="leading-tight">
